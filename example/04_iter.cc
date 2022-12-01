563cfce2a886546a495e09ae1388b2adb11d5352
chanchann <www.769440972@qq.com>
2022-12-01 09:51:19 +0800

    docs: array interface

---
+++ example/04_iter.cc
@@ -0,0 +1,45 @@
+#include "Json.h"
+#include <iostream>
+
+using namespace wfrest;
+
+void obj_iter()
+{
+    Json data;
+    data["key1"] = 1;
+    data["key2"] = 2.0;
+    data["key3"] = true;
+
+    for (Json::iterator it = data.begin(); it != data.end(); it++)
+    {
+        std::cout << it.key() << " : " << it.value() << std::endl;
+    }
+
+    for (const auto& it : data)
+    {
+        std::cout << it.key() << " : " << it.value() << std::endl;
+    }
+}
+
+void arr_iter()
+{
+    Json data;
+    data.push_back(1);
+    data.push_back(2.0);
+    data.push_back(false);
+    for (Json::iterator it = data.begin(); it != data.end(); it++)
+    {
+        // todo : iterator should behaves like pointer
+        std::cout << it.value() << std::endl;
+    }
+    for (const auto& it : data)
+    {
+        std::cout << it << std::endl;
+    }
+}
+
+int main()
+{
+    obj_iter();
+    arr_iter();
+}
