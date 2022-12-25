7db23a24c21eb431f816a09810a9f47e91b1be3e
Chanchan <www.769440972@qq.com>
2022-12-25 17:41:49 +0800

    example : reverse iterator example

--- example/04_iter.cc
+++ example/04_iter.cc
@@ -24,6 +24,23 @@ void obj_iter()
     }
 }
 
+void obj_iter_reverse()
+{
+    Json data;
+    data["key1"] = 1;
+    data["key2"] = 2.0;
+    data["key3"] = true;
+    for (Json::reverse_iterator it = data.rbegin(); it != data.rend(); it++)
+    {
+        std::cout << it->key() << " : " << it->value() << std::endl;
+    }
+    for (auto it = data.rbegin(); it != data.rend(); it++)
+    {
+        // equal to it->value()
+        std::cout << *it << std::endl;
+    }
+}
+
 void arr_iter()
 {
     Json data;
@@ -44,8 +61,34 @@ void arr_iter()
     }
 }
 
+void arr_iter_reverse()
+{
+    Json data;
+    data.push_back(1);
+    data.push_back(2.0);
+    data.push_back(false);
+    for (Json::reverse_iterator it = data.rbegin(); it != data.rend(); it++)
+    {
+        std::cout << it->value() << std::endl;
+    }
+    for (auto it = data.rbegin(); it != data.rend(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+}
+
 int main()
 {
+    printf("object iterator : \n");
     obj_iter();
+
+    printf("array iterator : \n");
     arr_iter();
+
+    printf("reverse object iterator : \n");
+    obj_iter_reverse();
+
+    printf("reverse array iterator : \n");
+    arr_iter_reverse();
+    return 0;
 }
